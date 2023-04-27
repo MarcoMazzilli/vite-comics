b462ace076c32f48556443b3ab1dd9388bfafd49
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-04-27 17:14:31 +0200

    [Add] Menu-db.js

---
+++ src/data/menus-db.js
@@ -0,0 +1,52 @@
+const headerLink = [
+    {
+        label:"characters",
+        url:"#",
+        active: false
+    },
+    {
+        label:"comics",
+        url:"#",
+        active: false
+    },
+    {
+        label:"movies",
+        url:"#",
+        active: false
+    },
+    {
+        label:"tv",
+        url:"#",
+        active: false
+    },
+    {
+        label:"games",
+        url:"#",
+        active: false
+    },
+    {
+        label:"collectibles",
+        url:"#",
+        active: false
+    },
+    {
+        label:"videos",
+        url:"#",
+        active: false
+    },
+    {
+        label:"fans",
+        url:"#",
+        active: false
+    },
+    {
+        label:"news",
+        url:"#",
+        active: false
+    },
+    {
+        label:"shop",
+        url:"#",
+        active: false
+    }
+]
