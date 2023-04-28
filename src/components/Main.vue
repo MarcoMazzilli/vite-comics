2ae1327c33f052791429ee7442815e7c7406ee56
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-04-28 18:06:44 +0200

    [add] card props

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,37 +1,52 @@
 <script>
+import "../components/film-card/Card.vue"
+import CardVue from './film-card/Card.vue'
+import arrayFilm from "../data/dc-comics.json"
+
 export default {
 data(){
     return{
-      message : "--> Content goes Here! <--"
+      arrayFilm,
     }
-}  
+  },  
+  components:{
+    CardVue,
+  }
 }
 </script>
 
 <template>
+
   <div class="main-wrapper">
-    
     <div class="container">
       
-      <h1>{{ message }}</h1>
+      <CardVue 
+      v-for="(film , index) in arrayFilm" :key="index" 
+      :img="film.thumb"
+      :text="film.series"
+      />
       
     </div>
-
   </div>
+
+
 </template>
+
 
 <style lang="scss" scoped>
 @use "../../src/scss/partials/vars" as * ;
 
 .main-wrapper{
+  min-height: $main-height;
   background-color: $bg-main;
-  
-  .container{
-    display: flex;
-    align-items: center;
-    height: $header-height;
-    color: white;
-  }
 
+  .container{
+    min-height: $main-height;
+    padding-top: 60px;
+    color: white;
+    display: flex;
+    flex-wrap: wrap;
+  }
 }
+
 </style>
---
+++ src/components/film-card/Card.vue
@@ -0,0 +1,54 @@
+<script>
+
+export default {
+    data(){
+        return{
+        }
+    },
+    props :{
+        img: String,
+        text: String
+    }
+   }
+</script>
+
+<template>
+
+      
+      <!-- -- -->
+      <div class="card">
+        <div class="card-img">
+          <img :src="img" :alt="text">
+        </div>
+        <div class="card-description">
+          <span>{{ text }}</span>
+        </div>
+      </div>
+      <!-- -- -->
+
+</template>
+
+<style lang="scss" scoped>
+@use "../../scss/partials/vars" as *;
+
+    .card{
+      width: calc(100%/6);
+      padding: 15px;
+    }
+    .card-img{
+      aspect-ratio: 2/3;
+      margin-bottom: 15px;
+      
+      img{
+        object-fit: cover;
+        object-position: center;
+        aspect-ratio: 2/3;
+      }
+    }
+    .card-description{
+      span{
+        text-transform: uppercase;
+      }
+    }
+
+</style>
